96cbe54cff795824cabfb2dea15775d614d2c133
R3alistic <acunhax@gmail.com>
2022-11-15 21:57:05 +0000

    fixed client ID and secret

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET
+    clientId: process.env.SPOTIFY_CLIENTID,
+    clientSecret: process.env.SPOTIFY_CLIENTSECRET
 });
 
 // Retrieve an access token
